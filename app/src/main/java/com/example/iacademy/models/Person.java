3aeb6e94f8a3ea1c81269d34f66aa678633d1af3
YerayLinares <116265030+YerayLinares@users.noreply.github.com>
2023-10-30 12:41:04 +0100

    Clase Persona
    Añadido la clase persona al proyecto

---
+++ app/src/main/java/com/example/iacademy/models/Person.java
@@ -0,0 +1,47 @@
+package com.example.iacademy.models;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+
+@Entity
+public class Person {
+
+    @ColumnInfo(name = "name")
+    private String name;
+
+    @ColumnInfo(name = "surNames")
+    private  String surNames;
+
+    @ColumnInfo(name = "email")
+    private String email;
+
+    public Person(String name, String surNames, String email) {
+        this.name = name;
+        this.surNames = surNames;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurNames() {
+        return surNames;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurNames(String surNames) {
+        this.surNames = surNames;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
